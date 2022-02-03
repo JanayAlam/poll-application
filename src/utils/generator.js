79d2e38b97afe0ab120639d444996f63665149ca
JanayAlam <janayalam820@gmail.com>
2022-02-03 15:27:00 +0600

    feat: Implemented a code or token generator

---
+++ src/utils/generator.js
@@ -0,0 +1,16 @@
+/**
+ * Generate a code of a desire size.
+ * @param {int} size Size of the code string.
+ * @returns {string} The generated code.
+ */
+const generateCode = (size) => {
+    const KEYS = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
+    let code = '#';
+    for (let i = 0; i < size; i++) {
+        code += characters.charAt(Math.floor(Math.random() *
+            KEYS.length));
+    }
+    return code;
+}
+
+module.exports = { generateCode };
