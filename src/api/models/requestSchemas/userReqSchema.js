d3b0cf744a03fb36d16318ee35caf5cbba950ac0
JanayAlam <janayalam820@gmail.com>
2022-02-03 16:05:12 +0600

    refactor: Refactored the schema folder

--- src/api/models/requestSchemas/userReqSchema.js
+++
@@ -1,8 +0,0 @@
-// Dependencies.
-const Joi = require('joi');
-
-const userReqSchema = Joi.object({
-    username: Joi.string().min(4).max(10).required(),
-    password: Joi.string().min(6).required(),
-});
-
