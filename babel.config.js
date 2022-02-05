cbd865a6871f92ecf04be61c4e072c7fdacd107a
JanayAlam <janayalam820@gmail.com>
2022-02-05 20:02:08 +0600

    feat: Added test functionality initially

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+// Configuration of babel.
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current'
+                }
+            }
+        ]
+    ],
+};
