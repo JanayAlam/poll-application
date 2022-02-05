b4608146747714873fce3e7110e10d9fb4b1c990
JanayAlam <janayalam820@gmail.com>
2022-02-05 20:41:53 +0600

    feat: Implemented get users feature

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -1,5 +1,6 @@
 // Dependencies.
 const bcrypt = require('bcrypt');
+const { NotFoundError } = require('../errors/apiErrors');
 
 // Importing models.
 const { User } = require('../models/data-models');
@@ -26,8 +27,11 @@ class UserService {
      * Get all the users from the database.
      * @returns Array of user objects.
      */
-    getAll = () => {
-        return [];
+    getAll = async () => {
+        // Fetching all the users from the database.
+        const users = await User.find();
+        // Returning the list of users.
+        return users;
     };
 
     /**
@@ -35,8 +39,13 @@ class UserService {
      * @param {int} id Id of the user.
      * @returns {User} The desire user object.
      */
-    get = (id) => {
-        return {};
+    get = async (id) => {
+         // Fetching all the users from the database.
+         const user = await User.findById(id);
+         // If the user is not found in the database.
+         if (!user) throw NotFoundError('User not found with the provided id.')
+         // Returning the list of users.
+         return user;
     };
 
     /**
