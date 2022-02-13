9cbc8c63ebd6cf777873ac46bcae4d68f4e46bab
JanayAlam <janayalam820@gmail.com>
2022-02-13 13:22:46 +0600

    feat: Changed the id to a valid mongoose object id

--- src/api/services/__mocks__/userService.js
+++ src/api/services/__mocks__/userService.js
@@ -1,10 +1,15 @@
+// Dependencies.
+import mongoose from 'mongoose';
+// Modules.
+import { NotFoundError } from '../../errors/apiErrors';
 // Importing models.
 import models from '../../models/data-models';
+
 
 // Local DB.
 let users = [
     {
-        _id: '7891q6w5e2r0tyu3i4plk1j1',
+        _id: '594ced02ed345b2b049222c5',
         username: 'username01',
         createdAt: Date.now(),
         modifiedAt: Date.now(),
@@ -13,7 +18,7 @@ let users = [
 ];
 
 /**
- * Save a user into the database.
+ * Save a user mock function.
  * @param {models.User} user The object that will be stored.
  * @returns {models.User} Created user object.
  */
@@ -28,10 +33,28 @@ export const store = async user => {
 };
 
 /**
- * Get all the users from the database.
+ * Get all the users mock function.
  * @returns {Array} Array of user objects.
  */
 export const getAll = async () => {
     // Returning the list of users.
     return users;
+};
+
+/**
+ * Get user by id mock function.
+ * @param {mongoose.ObjectId} id Id of the user.
+ * @returns {models.User} The desire user object.
+ */
+export const get = async id => {
+    // The provided id must be valid.
+    if (!mongoose.isValidObjectId(id)) {
+        throw new NotFoundError('User not found with the provided id.');
+    }
+    // Fetching all the users from the database.
+    const user = users.find(element => element._id === id);
+    // If the user is not found in the database.
+    if (!user) throw new NotFoundError('User not found with the provided id.');
+    // Returning the users.
+    return user;
 };
