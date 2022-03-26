98c920e09fc17c351df9d9cf80146bf6d89ae98a
JanayAlam <janayalam820@gmail.com>
2022-03-27 00:25:40 +0600

    refactor: Removed a linespace

--- src/api/routes/authRouter.js
+++ src/api/routes/authRouter.js
@@ -8,7 +8,6 @@ import reqModels from '../models/request-models';
 // Router instance.
 const router = express.Router();
 
-
 // POST: /api/v1/auth/register.
 router.post('/register', validateBody(reqModels.registrationRequestModel), registerHandler);
 // POST: /api/v1/auth/login.
