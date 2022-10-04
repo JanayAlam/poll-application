94637f6e80de47a27507e104d41054a0853ae8f8
JanayAlam <dev.janay@outlook.com>
2022-10-04 22:40:48 +0600

    added not found error page

---
+++ client/src/pages/errors/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="status-code text-danger">404</h1>
+        <p class="error-msg"><span class="text-danger">Ops!</span> Page not found. Please enter a valid url.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.status-code {
+    text-align: center;
+    font-size: 15rem;
+    font-weight: bold;
+}
+.error-msg {
+    text-align: center;
+    font-size: 1.2rem;
+    color: #333;
+    font-weight: 600;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import Login from '../pages/auth/Login.vue';
 import Register from '../pages/auth/Register.vue';
+import NotFound from '../pages/errors/NotFound.vue';
 import Home from '../pages/Home.vue';
 
 const routes = [
@@ -14,26 +15,33 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login,
-        beforeEnter: (_to, _from) => {
+        beforeEnter: (_to, from) => {
             if (!!localStorage.getItem('token')) {
-                return false;
+                return { name: from.name};
             }
+            return true;
         },
     },
     {
         path: '/register',
         name: 'register',
         component: Register,
-        beforeEnter: (_to, _from) => {
+        beforeEnter: (_to, from) => {
             if (!!localStorage.getItem('token')) {
-                return false;
+                return { name: from.name};
             }
+            return true;
         },
+    },
+    {
+        path: '/:catchAll(.*)*',
+        name: 'NotFound',
+        component: NotFound,
     },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 });
 
