43b0ed39be1826dc7c48d5411554dd117a519565
JanayAlam <dev.janay@outlook.com>
2022-10-14 18:31:12 +0600

    feat: setting up the home view UI

---
+++ client/src/components/home/ListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+        <li>item 4</li>
+    </ul>
+</template>
+
+<script>
+
+
+export default {
+    name: 'ListItem',
+    props: ['items'],
+    setup() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,28 +1,48 @@
 <template>
-    <div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-            voluptas hic obcaecati consectetur ab minus id modi necessitatibus
-            at? Eos aspernatur a voluptate eveniet saepe architecto voluptatibus
-            dolores? Officia accusamus deleniti quo tenetur dignissimos commodi,
-            aliquid culpa soluta ut quaerat, mollitia corporis repellat ipsum
-            illum sapiente numquam debitis atque magni sequi explicabo
-            accusantium unde libero? At excepturi vero, quasi, incidunt nobis
-            dolorum nostrum consequatur cupiditate minima odit ipsum autem
-            repellendus quam, eveniet earum asperiores ab vel minus ad velit
-            quisquam laboriosam enim eius perspiciatis? Illum aperiam qui illo
-            quia!
-        </p>
+    <div class="row mt-2">
+        <div class="col-md-3">
+            <list-item :items="sidebarItems" />
+        </div>
+        <div class="col-md-9">
+            <el-card
+                v-show="isAuthenticated"
+                shadow="hover"
+                class="create-poll-card"
+            >
+                <div class="create-poll-card-inner">
+                    <div>asda</div>
+                    <div>asdasd</div>
+                </div>
+            </el-card>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'Home',
-    setup() {
-        return {};
-    },
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import ListItem from '../components/home/ListItem.vue';
+
+document.title = 'Poll application';
+
+const store = useStore();
+const user = computed(() => store.getters.GET_USER);
+const isAuthenticated = computed(() => store.getters.GET_IS_AUTHENTICATED);
+
+const sidebarItems = {
+    filter: [],
+    topic: [],
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.create-poll-card {
+    cursor: pointer;
+    border-radius: 100px;
+    padding: 0;
+}
+.create-poll-card-inner {
+    display: flex;
+    gap: 1rem;
+}
+</style>
